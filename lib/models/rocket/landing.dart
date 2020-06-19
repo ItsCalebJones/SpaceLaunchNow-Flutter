2016f5f3bd6e53aea94948eaf85f19dd40ba0ce8
Caleb Jones <ca.jones9119@gmail.com>
2020-06-19 09:51:00 -0400

    Fix a bug with null landing info.

--- lib/models/rocket/landing.dart
+++ lib/models/rocket/landing.dart
@@ -11,12 +11,21 @@ class Landing {
   Landing({this.description, this.attempt, this.success, this.location, this.type});
 
   factory Landing.fromJson(Map<String, dynamic> json) {
+    var location;
+    if (json['location'] != null) {
+      location = new LandingLocation.fromJson(json['location']);
+    }
+
+    var type;
+    if (json['type'] != null) {
+      type = new LandingType.fromJson(json['type']);
+    }
     return Landing(
       description: json['description'],
       attempt: json['attempt'],
       success: json['success'],
-      location: new LandingLocation.fromJson(json['location']),
-      type: new LandingType.fromJson(json['type']),
+      location: location,
+      type: type,
     );
   }
 }
